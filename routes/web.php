8c98dbe4eaac5966eb122f52a7f572d04b7e6045
RbnCaro <rub.caro@hotmail.com>
2018-07-04 19:06:03 -0600

    Key para testing

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,8 @@ Route::get('/encuestas', 'EncuestaController@seeEncuestas')->name('survey');
 Route::get('/crear-encuesta', 'EncuestaController@seeCreateEncuesta')->name('seeCreateEncuesta');
 Route::get('/encuestas/{id}/resultados', 'EncuestaController@seeResults')->name('seeResults');
 Route::get('/encuestas/{id}', 'EncuestaController@seeEncuesta')->name('seeEncuesta');
+
+// Testing
+Route::get('/loaderio-308a469d9826b163fc4bfcb251f40c19', function() {
+    return 'loaderio-308a469d9826b163fc4bfcb251f40c19';
+  });
